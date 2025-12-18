7e384e9fa012a52c436c506d0840320c70542b9c
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-12-19 01:27:07 +0530

    add animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,6 @@ import HeroImage from "../assets/heroimages.jpg";
 const Hero = () => {
   return (
     <section className="relative min-h-screen overflow-hidden text-center">
-
       {/* Background Image */}
       <img
         src={HeroImage}
@@ -12,12 +11,27 @@ const Hero = () => {
         className="absolute inset-0 w-full h-full object-cover hero-zoom"
       />
 
+      {/* Dust particles */}
+      <div className="absolute inset-0 overflow-hidden z-5">
+        {[...Array(50)].map((_, i) => (
+          <span
+            key={i}
+            className="dust"
+            style={{
+              left: `${Math.random() * 100}%`,
+              bottom: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random() * 10}s`,
+              animationDuration: `${10 + Math.random() * 10}s`,
+            }}
+          ></span>
+        ))}
+      </div>
+
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/40"></div>
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-6">
-
         <h1 className="text-5xl md:text-6xl font-bold text-[#C89B3C] mb-4 fade-up delay-1">
           Capturing Your Precious Moments
         </h1>
@@ -28,12 +42,13 @@ const Hero = () => {
         </p>
 
         <Link to="/contact" className="fade-up delay-3">
-          <button className="bg-[#C89B3C] cursor-pointer text-[#0F0F0F] px-8 py-3 rounded-md font-semibold 
-          hover:scale-102 hover:shadow-xl transition duration-300">
+          <button
+            className="bg-[#C89B3C] cursor-pointer text-[#0F0F0F] px-8 py-3 rounded-md font-semibold 
+          hover:scale-102 hover:shadow-xl transition duration-300"
+          >
             Book a Session
           </button>
         </Link>
-
       </div>
     </section>
   );
