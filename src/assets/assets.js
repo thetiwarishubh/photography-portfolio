4a5dec66a224789cddf2ec093a7aeb53d74e3b28
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-01-03 21:20:07 +0530

    update code

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,9 +2,9 @@ import wedding from "./wedding.jpg";
 import cinematic from "./cinematic.jpg";
 import event from "./events.jpg";
 import owner from "./owner.jpg"
-import hero1 from "../assets/hero1.jpg"
-import hero4 from "../assets/hero4.jpg"
-import hero5 from "../assets/hero5.jpg"
+import hero1 from "./hero1.jpg"
+import hero4 from "./hero4.jpg"
+import hero5 from "./hero5.jpg"
 
 export {
   wedding,
