26fbb3d397c31c350acb0fb45fc94ee6cc963e1d
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-01-03 21:21:54 +0530

    add somethings

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,8 +3,8 @@ import cinematic from "./cinematic.jpg";
 import event from "./events.jpg";
 import owner from "./owner.jpg"
 import hero1 from "./hero1.jpg"
-import hero4 from "./hero4.jpg"
-import hero5 from "./hero5.jpg"
+import hero4 from "./hero4.JPG"
+import hero5 from "./hero5.JPG"
 
 export {
   wedding,
